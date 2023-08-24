2e05e29b177c4e3827ac7c007abd5b241b575214
Abhishek Chakraborty <54463117+obhishekbatif@users.noreply.github.com>
2023-08-24 18:20:59 +0530

    Update docker_network.sh

--- docker_network.sh
+++ docker_network.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
-NETWORK_NAME="bridge"
+NETWORK_NAME="mynetwork"
 
 # Get a list of all container IDs in the specified network
 container_ids=$(docker network inspect --format='{{range $id, $c := .Containers}}{{$id}} {{end}}' "$NETWORK_NAME")
 
-# Loop through each container ID and curl a specific endpoint
+# Loop through each container ID and curl a generic URL
 for container_id in $container_ids; do
     # Get the container's IP address
     container_ip=$(docker inspect --format '{{ .NetworkSettings.Networks.'$NETWORK_NAME'.IPAddress }}' $container_id)
 
-    # Define the port and endpoint to curl
+    # Define the port
     port=80  # Change to the appropriate port
-    #endpoint="/path/to/your/endpoint"  # Change to the appropriate endpoint
 
-    # Curl the endpoint in the current container
+    # Curl a generic URL in the current container
     curl_result=$(docker exec $container_id curl -s "http://$container_ip:$port")
 
     # Print the container ID and curl result
